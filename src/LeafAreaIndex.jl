7d949142ec1087eff163e440af788959863c8079
Ken-B <ken.bastiaensen@gmail.com>
2015-03-25 14:15:27 +0100

    add require file and include all methods

--- src/LeafAreaIndex.jl
+++ src/LeafAreaIndex.jl
@@ -1,10 +1,21 @@
 module LeafAreaIndex
 
-export calibrate, PolarImage, pixels
+import FixedPointNumbers: Ufixed
+import StreamStats
+
+using Lexicon
+
+export calibrate, PolarImage, pixels, gapfraction, contactfreq,
+        zenith57, miller
+
+# default ring width for zenith57 and contactfreq
+const RING_WIDTH = 5/180*π
 
 include("CameraLens.jl")
 include("PolarImage.jl")
 include("PolarRings.jl")
 include("PolarPixels.jl")
+include("thresholding.jl")
+include("inversion.jl")
 
 end # module
