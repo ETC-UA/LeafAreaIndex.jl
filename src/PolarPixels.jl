fdf4c9eceb1105bf56692d99895ee1f9369a8c65
Ken-B <ken.bastiaensen@gmail.com>
2015-03-29 23:19:15 +0200

    use array view for pixels method

--- src/PolarPixels.jl
+++ src/PolarPixels.jl
@@ -23,6 +23,18 @@ Base.next{T,A}(pc::PolarPixels{T,A}, i) = (pc.polim.imgsort[i]::T, i+1)
 
 Base.length(pc::PolarPixels) = pc.ind_last - pc.ind_first + 1
 Base.endof(pc::PolarPixels) = pc.polim.imgsort[pc.ind_last]
-pixels{T,A}(polim::PolarImage{T,A}, θ1::Real, θ2::Real) = collect(T, PolarPixels(polim,θ1,θ2))
+#pixels{T,A}(polim::PolarImage{T,A}, θ1::Real, θ2::Real) = collect(T, PolarPixels(polim,θ1,θ2))
+
+using ArrayViews
+
+# for just polar distance, returning a view is sufficient and much faster
+function pixels(polim::PolarImage, θ1::Real, θ2::Real) 
+    θ1 < 0 && throw(DomainError())
+    θ2 > π/2 && throw(DomainError())
+    ind_first = searchsortedfirst(polim.cl.ρ²sort, polim.cl.fθρ(θ1)^2)
+    ind_last = searchsortedlast(polim.cl.ρ²sort, polim.cl.fθρ(θ2)^2) 
+	view(polim.imgsort, ind_first:ind_last)
+end
+
 pixels(polim::PolarImage, θ::Real) = pixels(polim, zero(θ), θ)
 pixels(polim::PolarImage) = pixels(polim, π/2)
