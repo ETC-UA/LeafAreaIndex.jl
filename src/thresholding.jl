10c2bcf7ef13ecc01544062a76e1f1c25772f248
Ken-B <ken.bastiaensen@gmail.com>
2015-03-30 12:51:38 +0200

    remove slow RidlerCalvard

--- src/thresholding.jl
+++ src/thresholding.jl
@@ -1,34 +1,16 @@
 
-threshold(gray) = RidlerCalvardfast(gray)
+threshold(gray) = RidlerCalvard(gray)
 threshold(polim::PolarImage) = threshold(pixels(polim))
 
 function RidlerCalvard(gray)
     max_iter = 100
     tol = 1e-6
 
-    thresh = mean(gray)
-    count = 1
-    while count < max_iter
-        whites = gray .> thresh
-        high = mean(gray[whites])
-        low = mean(gray[~whites])
-        thresh_old = copy(thresh)
-        thresh = (high + low)/2
-        abs(thresh - thresh_old) < tol && break
-        count += 1
-    end
-    return(thresh)
-end
-
-#four times faster
-function RidlerCalvardfast(gray)
-    max_iter = 100
-    tol = 1e-6
-
-    thresh = mean(gray)
+    @show thresh = mean(gray)
     count = 1
 
-    # single pass over input for both high and low mean without temporary array allocation
+    # Single pass over input for both high and low mean is
+    # almost 5 times faster if no temporary array allocation.
     function highlowmean(gray, thresh)
         lowmean = highmean = zero(thresh)
         lowcount = highcount = 0
